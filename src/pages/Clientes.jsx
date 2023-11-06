0039a9ee1063283c77e10d892a62c6aa7cffdb47
CampiV <144741798+CampiV@users.noreply.github.com>
2023-11-05 22:37:28 -0300

    Actualizacion 2

--- src/pages/Clientes.jsx
+++ src/pages/Clientes.jsx
@@ -1,28 +1,170 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 export default function Clientes() {
-return (
-    <div className="content-wrapper">
-        <div class="card bg-light d-flex flex-fill">
-<div class="card-header text-muted border-bottom-0">
-Codigo Cliente: Backend 
-</div>
-<div class="card-body pt-0">
-<div class="row">
-<div class="col-7">
-<h2 class="lead"><b>Claudio Acevedo</b></h2>
-<p class="text-muted text-sm"><b>About: </b> Gay / Maquina / Cantante / Sexsologo </p>
-<ul class="ml-4 mb-0 fa-ul text-muted">
-<li class="small"><span class="fa-li"><i class="fas fa-lg fa-building"></i></span> Address: Demo Street 123, Demo City 04312, NJ</li>
-<li class="small"><span class="fa-li"><i class="fas fa-lg fa-phone"></i></span> Phone #: + 800 - 12 12 23 52</li>
-</ul>
-</div>
-<div class="col-5 text-center">
-</div>
-</div>
-</div>
+  const [clients, setClients] = useState([
+    {
+      nombre: 'Claudio',
+      RUT: '20.402.593-2',
+      Cod_Cliente: 100,
+      Telefono: '+56 9 4577 0230',
+      Direccion:'Talca, Region del Maule, Chile, 3465008',
+    }
+  ]);
 
-</div>
+  const [editIndex, setEditIndex] = useState(-1);
+  const [editClients, setEditClients] = useState({
+    nombre: '',
+    RUT: '',
+    Cod_Cliente: '',
+    Telefono: '',
+    Direccion:'',
+  });
+
+  const [cart, setCart] = useState([]);
+  const [unitCount, setUnitCount] = useState(0);
+
+  const agregarClientes = () => {
+    const nuevoCliente = {
+      nombre: 'Nuevo Cliente',
+      RUT: '12.345.678.-9',
+      Cod_Cliente: '###',
+      Telefono: '+56 912345678',
+      Direccion: 'Region, Ciudad, Poblacion, Calle, Numero',
+    };
+    setClients([...clients, nuevoCliente]);
+  };
+
+  const eliminarCliente = (index) => {
+    const nuevosClientes = [...clients];
+    nuevosClientes.splice(index, 1);
+    setClients(nuevosClientes);
+  };
+
+  const editarClientes = (index) => {
+    setEditIndex(index);
+    setEditClients(clients[index]);
+  };
+
+  const guardarClienteEditado = () => {
+    const nuevosClientes = [...clients];
+    nuevosClientes[editIndex] = editClients;
+    setClients(nuevosClientes);
+    setEditIndex(-1);
+    setEditClients({ nombre: '', RUT: '', Cod_Cliente: 0 });
+  };
+
+  return (
+    <div className="content-wrapper">
+      <div className="card">
+        <div className="card-header border-0">
+          <h3 className="card-title">CLIENTES DE VINOS DON ROGELIO</h3>
+          <div className="card-tools">
+            <button onClick={agregarClientes} className="btn btn-primary">
+              Agregar
+            </button>
+          </div>
+        </div>
+        <div className="card-body table-responsive p-0">
+          <table className="table table-striped table-valign-middle">
+            <thead>
+              <tr>
+                <th>Nombre</th>
+                <th>RUT</th>
+                <th>COD_Cliente</th>
+                <th>Telefono</th>
+                <th>Direccion</th>
+              </tr>
+            </thead>
+            <tbody>
+              {clients.map((client, index) => (
+                <tr key={index}>
+                  <td>
+                    {editIndex === index ? (
+                      <input
+                        type="text"
+                        value={editClients.nombre}
+                        onChange={(e) =>
+                          setEditClients({ ...editClients, nombre: e.target.value })
+                        }
+                      />
+                    ) : (
+                      client.nombre
+                    )}
+                  </td>
+                  <td>
+                    {editIndex === index ? (
+                      <input
+                        type="text"
+                        value={editClients.RUT}
+                        onChange={(e) =>
+                          setEditClients({ ...editClients, RUT: e.target.value })
+                        }
+                      />
+                    ) : (
+                      client.RUT
+                    )}
+                  </td>
+                  <td>
+                    {editIndex === index ? (
+                      <input
+                        type="number"
+                        value={editClients.Cod_Cliente}
+                        onChange={(e) =>
+                          setEditClients({ ...editClients, Cod_Cliente: e.target.value })
+                        }
+                      />
+                    ) : (
+                      client.Cod_Cliente
+                    )}
+                  </td>
+                  <td>
+                    {editIndex === index ? (
+                      <input
+                        type="text"
+                        value={editClients.Telefono}
+                        onChange={(e) =>
+                          setEditClients({ ...editClients, Telefono: e.target.value })
+                        }
+                      />
+                    ) : (
+                      client.Telefono
+                    )}
+                  </td>
+                  <td>
+                    {editIndex === index ? (
+                      <input
+                        type="text"
+                        value={editClients.Direccion}
+                        onChange={(e) =>
+                          setEditClients({ ...editClients, Direccion: e.target.value })
+                        }
+                      />
+                    ) : (
+                      client.Direccion
+                    )}
+                  </td>
+                  <td>
+                    {editIndex === index ? (
+                      <button onClick={guardarClienteEditado} className="btn btn-success">
+                        Guardar
+                      </button>
+                    ) : (
+                      <>
+                        <button onClick={() => editarClientes(index)} className="btn btn-info">
+                          Editar
+                        </button>
+                        <button onClick={() => eliminarCliente(index)} className="btn btn-danger">
+                          Eliminar
+                        </button>
+                      </>
+                    )}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
-)
+  );
 }
